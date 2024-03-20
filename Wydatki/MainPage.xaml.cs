4799e0c6b72b3372da6aaee200d104229fa10a50
Jakub Koza <kubulax25@gmail.com>
2024-03-20 09:49:57 +0100

    Dodaj pliki projektów.

---
+++ Wydatki/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Wydatki
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AddSpending(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Details(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
